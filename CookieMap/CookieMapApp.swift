f7ad82b212445f234ce38b0636a91aa8051bdb6c
hazi0930 <karin1203@naver.com>
2026-08-17 00:29:58 +0900

    Initial Commit

---
+++ CookieMap/CookieMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieMapApp.swift
+//  CookieMap
+//
+//  Created by 허지우 on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct CookieMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
